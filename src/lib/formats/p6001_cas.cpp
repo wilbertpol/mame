2fd017286dec289694225385bd86fceccf53b645
Wilbert Pol <wilbertpol@users.noreply.github.com>
2022-01-22 15:43:18 +0000

    p6001_cas.cpp: Remove LegacyWaveFiller

--- src/lib/formats/p6001_cas.cpp
+++ src/lib/formats/p6001_cas.cpp
@@ -9,65 +9,26 @@
 #define WAVE_HIGH        0x5a9e
 #define WAVE_LOW        -0x5a9e
 
-static int cas_size; // FIXME: global variable prevents multiple instances
 
-static int pc6001_fill_wave(int16_t* buffer, uint8_t data, int sample_pos)
+static void pc6001_fill_wave(std::vector<int16_t> &samples, uint8_t data)
 {
-	int sample_count = 0;
-
 	// one byte = 8 samples
-	for(int x=0;x<8;x++)
+	for (int x = 0; x < 8;x++)
 	{
-		if(buffer)
-			buffer[sample_pos+x] = ((data >> (7-x)) & 1) ? WAVE_HIGH : WAVE_LOW;
+		samples.push_back(((data >> (7-x)) & 1) ? WAVE_HIGH : WAVE_LOW);
 	}
 
-	sample_count+=8;
-	return sample_count;
 }
 
-static int pc6001_handle_cas(int16_t* buffer, const uint8_t* casdata)
-{
-	int sample_count = 0;
-	int data_pos = 0;
 
-	while(sample_count < cas_size)
+static void pc6001_handle_cas(std::vector<int16_t> &samples, std::vector<uint8_t> &bytes)
+{
+	for (int pos = 0; pos < bytes.size(); pos++)
 	{
-		sample_count += pc6001_fill_wave(buffer,casdata[data_pos],sample_count);
-		data_pos++;
+		pc6001_fill_wave(samples, bytes[pos]);
 	}
-
-	return sample_count;
 }
 
-/*******************************************************************
-   Calculate the number of samples needed for this tape image
-********************************************************************/
-static int pc6001_cas_to_wav_size (const uint8_t *casdata, int caslen)
-{
-	cas_size = caslen*8;
-
-	return pc6001_handle_cas(nullptr,casdata);
-}
-
-/*******************************************************************
-   Generate samples for the tape image
-********************************************************************/
-static int pc6001_cas_fill_wave(int16_t *buffer, int sample_count, uint8_t *bytes)
-{
-	return pc6001_handle_cas(buffer,bytes);
-}
-
-static const cassette_image::LegacyWaveFiller pc6001_legacy_fill_wave =
-{
-	pc6001_cas_fill_wave,                   /* fill_wave */
-	-1,                                     /* chunk_size */
-	0,                                      /* chunk_samples */
-	pc6001_cas_to_wav_size,                 /* chunk_sample_calc */
-	8000,                                   /* sample_frequency */
-	0,                                      /* header_samples */
-	0                                       /* trailer_samples */
-};
 
 static cassette_image::error pc6001_cas_identify(cassette_image *cassette, cassette_image::Options *opts)
 {
@@ -78,10 +39,18 @@ static cassette_image::error pc6001_cas_identify(cassette_image *cassette, casse
 }
 
 
-
 static cassette_image::error pc6001_cas_load(cassette_image *cassette)
 {
-	return cassette->legacy_construct(&pc6001_legacy_fill_wave);
+	uint64_t file_size = cassette->image_size();
+	std::vector<uint8_t> bytes(file_size);
+	cassette->image_read(&bytes[0], 0, file_size);
+	std::vector<int16_t> samples;
+
+	pc6001_handle_cas(samples, bytes);
+
+	return cassette->put_samples(0, 0.0,
+			(double)samples.size() / 8000, samples.size(), 2,
+			&samples[0], cassette_image::WAVEFORM_16BIT);
 }
 
 
@@ -91,6 +60,7 @@ static const cassette_image::Format pc6001_cassette_format = {
 	pc6001_cas_load,
 	nullptr
 };
+
 
 CASSETTE_FORMATLIST_START(pc6001_cassette_formats)
 	CASSETTE_FORMAT(pc6001_cassette_format)
