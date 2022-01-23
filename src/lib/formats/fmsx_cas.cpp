bae0d06da6ea8a35711634cad9060fe0d69e38a6
Wilbert Pol <wilbertpol@users.noreply.github.com>
2022-01-23 11:36:14 +0000

    fmsx_cas.cpp: Remove LegacyWaveFiller

--- src/lib/formats/fmsx_cas.cpp
+++ src/lib/formats/fmsx_cas.cpp
@@ -9,70 +9,33 @@
 #define CAS_PERIOD        (16)
 #define CAS_HEADER_PERIODS (4000)
 #define CAS_EMPTY_PERIODS (1000)
-static const uint8_t CasHeader[8] = { 0x1F,0xA6,0xDE,0xBA,0xCC,0x13,0x7D,0x74 };
 
-static int cas_size;
+static const uint8_t CAS_HEADER[8] = { 0x1F,0xA6,0xDE,0xBA,0xCC,0x13,0x7D,0x74 };
 
 
-/*******************************************************************
-   Calculate the number of samples needed for this tape image
-********************************************************************/
-static int fmsx_cas_to_wav_size (const uint8_t *casdata, int caslen)
+static void fmsx_cas_fill_wave(std::vector<int16_t> &samples, std::vector<uint8_t> &bytes)
 {
-	int     pos, size;
+	int state = 1;
+	int cas_pos = 0;
 
-	if (caslen < 8) return -1;
-	if (memcmp (casdata, CasHeader, sizeof (CasHeader) ) ) return -1;
-
-	pos = size = 0;
-
-	while (pos < caslen)
+	while (cas_pos < bytes.size())
 	{
-		if ( (pos + 8) < caslen)
-			if (!memcmp (casdata + pos, CasHeader, 8) )
-			{
-				size += (CAS_EMPTY_PERIODS + CAS_HEADER_PERIODS) * CAS_PERIOD;
-				pos += 8;
-				continue;
-			}
-
-		size += CAS_PERIOD * 12;
-		pos++;
-	}
-
-	cas_size = caslen;
-
-	return size;
-}
-
-
-/*******************************************************************
-   Generate samples for the tape image
-********************************************************************/
-static int fmsx_cas_fill_wave(int16_t *buffer, int sample_count, uint8_t *bytes)
-{
-	int cas_pos, bit, state = 1, samples_pos, size, n, i, p;
-
-	cas_pos = 0;
-	samples_pos = 0;
-
-	while (samples_pos < sample_count && cas_pos < cas_size)
-	{
-		/* Check if we need to output a header */
-		if ( cas_pos + 8 < cas_size )
+		// Check if we need to output a header
+		if (cas_pos + 8 < bytes.size())
 		{
-			if ( ! memcmp( bytes + cas_pos, CasHeader, 8 ) )
+			if (!memcmp(&bytes[cas_pos], CAS_HEADER, 8 ))
 			{
-				/* Write CAS_EMPTY_PERIODS of silence */
-				n = CAS_EMPTY_PERIODS * CAS_PERIOD; while (n--) buffer[samples_pos++] = 0;
+				// Write CAS_EMPTY_PERIODS of silence
+				int n = CAS_EMPTY_PERIODS * CAS_PERIOD;
+				while (n--)
+					samples.push_back(0);
 
-				/* Write CAS_HEADER_PERIODS of header (high frequency) */
-				for (i=0;i<CAS_HEADER_PERIODS*4;i++)
+				// Write CAS_HEADER_PERIODS of header (high frequency)
+				for (int i = 0; i <CAS_HEADER_PERIODS * 4; i++)
 				{
-					for (n=0;n<CAS_PERIOD / 4;n++)
-						buffer[samples_pos + n] = (state ? 32767 : -32767);
+					for (int n=0; n <CAS_PERIOD / 4;n++)
+						samples.push_back(state ? 32767 : -32767);
 
-					samples_pos += CAS_PERIOD / 4 ;
 					state = !state;
 				}
 
@@ -80,42 +43,27 @@ static int fmsx_cas_fill_wave(int16_t *buffer, int sample_count, uint8_t *bytes)
 			}
 		}
 
-		for (i=0;i<=11;i++)
+		for (int i=0; i <= 11; i++)
 		{
+			int bit = 0;
 			if (i == 0) bit = 0;
 			else if (i < 9) bit = (bytes[cas_pos] & (1 << (i - 1) ) );
 			else bit = 1;
 
-			/* write this one bit */
-			for (n=0;n<(bit ? 4 : 2);n++)
+			// write this one bit
+			for (int n = 0; n < (bit ? 4 : 2); n++)
 			{
-				size = (bit ? CAS_PERIOD / 4 : CAS_PERIOD / 2);
-				for (p=0;p<size;p++)
+				int size = (bit ? CAS_PERIOD / 4 : CAS_PERIOD / 2);
+				for (int p = 0; p < size;p++)
 				{
-					buffer[samples_pos + p] = (state ? 32767 : -32767);
+					samples.push_back((state ? 32767 : -32767));
 				}
 				state = !state;
-				samples_pos += size;
 			}
 		}
 		cas_pos++;
 	}
-
-	return samples_pos;
 }
-
-
-static const cassette_image::LegacyWaveFiller fmsx_legacy_fill_wave =
-{
-	fmsx_cas_fill_wave,                     /* fill_wave */
-	-1,                                     /* chunk_size */
-	0,                                      /* chunk_samples */
-	fmsx_cas_to_wav_size,                   /* chunk_sample_calc */
-	22050,                                  /* sample_frequency */
-	0,                                      /* header_samples */
-	0                                       /* trailer_samples */
-};
-
 
 
 static cassette_image::error fmsx_cas_identify(cassette_image *cassette, cassette_image::Options *opts)
@@ -127,12 +75,19 @@ static cassette_image::error fmsx_cas_identify(cassette_image *cassette, cassett
 }
 
 
-
 static cassette_image::error fmsx_cas_load(cassette_image *cassette)
 {
-	return cassette->legacy_construct(&fmsx_legacy_fill_wave);
-}
+	uint64_t file_size = cassette->image_size();
+	std::vector<uint8_t> bytes(file_size);
+	cassette->image_read(&bytes[0], 0, file_size);
+	std::vector<int16_t> samples;
 
+	fmsx_cas_fill_wave(samples, bytes);
+
+	return cassette->put_samples(0, 0.0,
+			(double)samples.size() / 22050, samples.size(), 2,
+			&samples[0], cassette_image::WAVEFORM_16BIT);
+}
 
 
 static const cassette_image::Format fmsx_cas_format =
@@ -142,7 +97,6 @@ static const cassette_image::Format fmsx_cas_format =
 	fmsx_cas_load,
 	nullptr
 };
-
 
 
 CASSETTE_FORMATLIST_START(fmsx_cassette_formats)
