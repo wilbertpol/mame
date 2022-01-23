206c28dd67e5d4beccaf92be48ab24509e19a147
Wilbert Pol <wilbertpol@users.noreply.github.com>
2022-01-23 12:38:30 +0000

    cgen_cas.cpp: Remove LegacyWaveFiller

--- src/lib/formats/cgen_cas.cpp
+++ src/lib/formats/cgen_cas.cpp
@@ -29,111 +29,67 @@ We now support these three types below...
 #define SMPHI   32767
 
 
-static int cas_size;
-static int level;
-
-
-static int cgenie_output_byte(int16_t *buffer, int sample_count, uint8_t data)
+static void cgenie_output_byte(std::vector<int16_t> &samples, bool &level, uint8_t data)
 {
-	int samples = 0;
-
 	for (int i = 0; i < 8; i++)
 	{
 		// Output bit boundary
-		level ^= 1;
-		if (buffer)
-			buffer[sample_count + samples] = level ? SMPHI : SMPLO;
-		samples++;
+		level = !level;
+		samples.push_back(level ? SMPHI : SMPLO);
 
 		// Output bit
 		if (data & 0x80)
-			level ^= 1;
-		if (buffer)
-			buffer[sample_count + samples] = level ? SMPHI : SMPLO;
-		samples++;
+			level = !level;
+		samples.push_back(level ? SMPHI : SMPLO);
 
 		data <<= 1;
 	}
-	return samples;
 }
 
 
-static int cgenie_handle_cas(int16_t *buffer, const uint8_t *casdata)
+static cassette_image::error cgenie_handle_cas(std::vector<int16_t> &samples, std::vector<uint8_t> &bytes)
 {
-	int data_pos, sample_count;
-
-	data_pos = 0;
-	sample_count = 0;
-	level = 0;
+	int data_pos = 0;
+	bool level = false;
 
 	// Check for presence of optional header
-	if (!memcmp(casdata, TAPE_HEADER, sizeof(TAPE_HEADER) - 1))
+	if (bytes.size() >= sizeof(TAPE_HEADER) && !memcmp(&bytes[0], TAPE_HEADER, sizeof(TAPE_HEADER) - 1))
 	{
 		// Search for 0x00 or end of file
-		while (data_pos < cas_size && casdata[data_pos])
+		while (data_pos < bytes.size() && bytes[data_pos])
 			data_pos++;
 
 		// If we're at the end of the file it's not a valid .cas file
-		if (data_pos == cas_size)
-			return -1;
+		if (data_pos == bytes.size())
+			return cassette_image::error::INVALID_IMAGE;
 
 		// Skip the 0x00 byte
 		data_pos++;
 	}
 
 	// If we're at the end of the file it's not a valid .cas file
-	if (data_pos == cas_size)
-		return -1;
+	if (data_pos == bytes.size())
+		return cassette_image::error::INVALID_IMAGE;
 
 	// Check for beginning of tape file marker (possibly skipping the 0xaa header)
-	if (casdata[data_pos] != 0x66 && casdata[data_pos + 0xff] != 0x66)
-		return -1;
+	if (bytes[data_pos] != 0x66 && data_pos + 0xff <= bytes.size() && bytes[data_pos + 0xff] != 0x66)
+		return cassette_image::error::INVALID_IMAGE;
 
 	// Create header, if not present in the file
-	if (casdata[data_pos] == 0x66)
+	if (bytes[data_pos] == 0x66)
 		for (int i = 0; i < 256; i++)
-			sample_count += cgenie_output_byte(buffer, sample_count, 0xaa);
+			cgenie_output_byte(samples, level, 0xaa);
 
 	// Start outputting data
-	while (data_pos < cas_size)
+	while (data_pos < bytes.size())
 	{
-		sample_count += cgenie_output_byte(buffer, sample_count, casdata[data_pos]);
+		cgenie_output_byte(samples, level, bytes[data_pos]);
 		data_pos++;
 	}
-	sample_count += cgenie_output_byte(buffer, sample_count, 0x00);
+	cgenie_output_byte(samples, level, 0x00);
 
-	return sample_count;
+	return cassette_image::error::SUCCESS;
 }
-
-/*******************************************************************
-   Generate samples for the tape image
-********************************************************************/
-static int cgenie_cas_fill_wave(int16_t *buffer, int sample_count, uint8_t *bytes)
-{
-	return cgenie_handle_cas(buffer, bytes);
-}
-
-
-/*******************************************************************
-   Calculate the number of samples needed for this tape image
-********************************************************************/
-static int cgenie_cas_to_wav_size(const uint8_t *casdata, int caslen)
-{
-	cas_size = caslen;
-
-	return cgenie_handle_cas(nullptr, casdata);
-}
-
-static const cassette_image::LegacyWaveFiller cgenie_cas_legacy_fill_wave =
-{
-	cgenie_cas_fill_wave,                   /* fill_wave */
-	-1,                                     /* chunk_size */
-	0,                                      /* chunk_samples */
-	cgenie_cas_to_wav_size,                 /* chunk_sample_calc */
-	2400,                                   /* sample_frequency */
-	0,                                      /* header_samples */
-	0                                       /* trailer_samples */
-};
 
 
 static cassette_image::error cgenie_cas_identify(cassette_image *cassette, cassette_image::Options *opts)
@@ -147,7 +103,18 @@ static cassette_image::error cgenie_cas_identify(cassette_image *cassette, casse
 
 static cassette_image::error cgenie_cas_load(cassette_image *cassette)
 {
-	return cassette->legacy_construct(&cgenie_cas_legacy_fill_wave);
+	uint64_t file_size = cassette->image_size();
+	std::vector<uint8_t> bytes(file_size);
+	cassette->image_read(&bytes[0], 0, file_size);
+	std::vector<int16_t> samples;
+
+	cassette_image::error err = cgenie_handle_cas(samples, bytes);
+	if (err != cassette_image::error::SUCCESS)
+		return err;
+
+	return cassette->put_samples(0, 0.0,
+			(double)samples.size() / 2400, samples.size(), 2,
+			&samples[0], cassette_image::WAVEFORM_16BIT);
 }
 
 
