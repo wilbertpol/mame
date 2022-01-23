28b6c5943b59691b516c3b0685eed641d1e85b01
Wilbert Pol <wilbertpol@users.noreply.github.com>
2022-01-23 16:07:48 +0000

    orao_cas.cpp: Remove LegacyWaveFiller

--- src/lib/formats/orao_cas.cpp
+++ src/lib/formats/orao_cas.cpp
@@ -18,94 +18,50 @@
 
 #define ORAO_HEADER_SIZE 360
 
-static int16_t    wave_data; // FIXME: global variables prevent multiple instances
-static int        len;
-static int        startpos;
-static bool       newformat;
 
-static void orao_output_wave( int16_t **buffer, int length ) {
-	if ( buffer == nullptr ) {
-		return;
-	}
-
-	for( ; length > 0; length-- ) {
-		**buffer = wave_data;
-		*buffer = *buffer + 1;
-	}
+static void orao_output_wave(std::vector<int16_t> &samples, int16_t wave_data, int length)
+{
+	for ( ; length > 0; length--)
+		samples.push_back(wave_data);
 }
-static int orao_cas_to_wav_size( const uint8_t *casdata, int caslen ) {
-	int i,j,size;
-	uint8_t b;
 
-	if (casdata == nullptr) return -1;
-	if (caslen <= ORAO_HEADER_SIZE) {
-		return -1;
-	}
-	size = 0;
-	startpos = 0;
-	newformat = true;
-	if (casdata[0]==0x68 && casdata[1]==0x01 && casdata[2]==0x00) {
+
+static void orao_cas_fill_wave(std::vector<int16_t> &samples, std::vector<uint8_t> &bytes)
+{
+	int startpos = 0;
+	bool newformat = true;
+
+	if (bytes[0]==0x68 && bytes[1]==0x01 && bytes[2]==0x00)
+	{
 		startpos = ORAO_HEADER_SIZE;
 		newformat = false;
 	}
-	for (i=startpos;i<caslen-startpos;i++) {
-		for (j=0;j<8;j++) {
-			b = (casdata[i] >> j) & 1;
-			if (b==0) {
-				size += 2*ORAO_WAVE_ZERO;
-			} else {
-				size += 2*ORAO_WAVE_ONE;
+	for (int i = startpos; i < bytes.size(); i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			int k = newformat ? (7 - j) : j;
+			uint8_t b = (bytes[i] >> k) & 1;
+			if (b == 0)
+			{
+				orao_output_wave(samples, WAVE_LOW, ORAO_WAVE_ZERO);
+				orao_output_wave(samples, WAVE_HIGH, ORAO_WAVE_ZERO);
+			}
+			else
+			{
+				orao_output_wave(samples, WAVE_LOW, ORAO_WAVE_ONE);
+				orao_output_wave(samples, WAVE_HIGH, ORAO_WAVE_ONE);
 			}
 		}
 	}
-	len = caslen;
-	return size;
-}
-
-static int orao_cas_fill_wave( int16_t *buffer, int length, uint8_t *bytes ) {
-	int i,j,size,k;
-	uint8_t b;
-	size = 0;
-	if (bytes == nullptr) return -1;
-	for (i=startpos;i<len-startpos;i++) {
-		for (j=0;j<8;j++) {
-			k = newformat ? (7-j) : j;
-			b = (bytes[i] >> k) & 1;
-			if (b==0) {
-				wave_data = WAVE_LOW;
-				orao_output_wave(&buffer,ORAO_WAVE_ZERO);
-				wave_data = WAVE_HIGH;
-				orao_output_wave(&buffer,ORAO_WAVE_ZERO);
-				size += 2 *ORAO_WAVE_ZERO;
-			} else {
-				wave_data = WAVE_LOW;
-				orao_output_wave(&buffer,ORAO_WAVE_ONE);
-				wave_data = WAVE_HIGH;
-				orao_output_wave(&buffer,ORAO_WAVE_ONE);
-				size += 2 * ORAO_WAVE_ONE;
-			}
-		}
-	}
-
-	return size;
 }
 
 
+static cassette_image::error orao_cassette_identify(cassette_image *cassette, cassette_image::Options *opts)
+{
+	if (cassette->image_size() < ORAO_HEADER_SIZE)
+		return cassette_image::error::INVALID_IMAGE;
 
-
-static const cassette_image::LegacyWaveFiller orao_legacy_fill_wave = {
-	orao_cas_fill_wave,         /* fill_wave */
-	-1,                 /* chunk_size */
-	0,                  /* chunk_samples */
-	orao_cas_to_wav_size,           /* chunk_sample_calc */
-	ORAO_WAV_FREQUENCY,         /* sample_frequency */
-	0,                  /* header_samples */
-	0                   /* trailer_samples */
-};
-
-
-
-static cassette_image::error orao_cassette_identify( cassette_image *cassette, cassette_image::Options *opts ) {
 	opts->channels = 1;
 	opts->bits_per_sample = 16;
 	opts->sample_frequency = ORAO_WAV_FREQUENCY;
@@ -113,14 +69,26 @@ static cassette_image::error orao_cassette_identify( cassette_image *cassette, c
 }
 
 
+static cassette_image::error orao_cassette_load(cassette_image *cassette)
+{
+	uint64_t file_size = cassette->image_size();
+	std::vector<uint8_t> bytes(file_size);
+	cassette->image_read(&bytes[0], 0, file_size);
+	std::vector<int16_t> samples;
 
-static cassette_image::error orao_cassette_load( cassette_image *cassette ) {
-	return cassette->legacy_construct( &orao_legacy_fill_wave );
+	if (file_size < ORAO_HEADER_SIZE)
+		return cassette_image::error::INVALID_IMAGE;
+
+	orao_cas_fill_wave(samples, bytes);
+
+	return cassette->put_samples(0, 0.0,
+			(double)samples.size() / ORAO_WAV_FREQUENCY, samples.size(), 2,
+			&samples[0], cassette_image::WAVEFORM_16BIT);
 }
 
 
-
-static const cassette_image::Format orao_cassette_format = {
+static const cassette_image::Format orao_cassette_format =
+{
 	"tap",
 	orao_cassette_identify,
 	orao_cassette_load,
