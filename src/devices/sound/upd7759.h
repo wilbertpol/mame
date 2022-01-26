d1430c9892da257d8ef05f07bc92b103c421f980
AJR <ajrhacker@users.noreply.github.com>
2022-01-26 08:47:06 -0500

    Remove void *ptr parameter from emu_timer, timer_device and all related callbacks

--- src/devices/sound/upd7759.h
+++ src/devices/sound/upd7759.h
@@ -138,7 +138,7 @@ protected:
 
 	virtual void device_start() override;
 	virtual void device_reset() override;
-	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
+	virtual void device_timer(emu_timer &timer, device_timer_id id, int param) override;
 
 	void internal_md_w(int state);
 
