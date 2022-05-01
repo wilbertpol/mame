19e2c2cd16c974b0bf2660982d1ae53039f0a5ca
hap <happppp@users.noreply.github.com>
2022-05-01 23:07:52 +0200

    New working machines -------------------- Scrabble Lexor - Computer Word Game [hap, Sean Riddle]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -1404,6 +1404,7 @@ function linkProjects_mame_mess(_target, _subtarget)
 		"sanyo",
 		"saturn",
 		"segacons",
+		"selchow",
 		"sequential",
 		"sgi",
 		"sharp",
@@ -3688,6 +3689,11 @@ files {
 	MAME_DIR .. "src/mame/machine/megacd.h",
 	MAME_DIR .. "src/mame/machine/megacdcd.cpp",
 	MAME_DIR .. "src/mame/machine/megacdcd.h",
+}
+
+createMESSProjects(_target, _subtarget, "selchow")
+files {
+	MAME_DIR .. "src/mame/drivers/scrablex.cpp",
 }
 
 createMESSProjects(_target, _subtarget, "sequential")
